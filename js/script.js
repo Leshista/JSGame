ec584108b5855ad7802c4b8effcbf066a9a83e99
leshista <a.leshista@gmail.com>
2021-11-09 12:55:40 +0300

    Added the "Start Game" menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const startButton = document.querySelector('.game-start__button');
+const backgroundMusic = document.querySelector('.background-music');
+
+
+// startButton.addEventListener('click', function () {
+//     startGame();
+//     backgroundMusic.play();
+// });
