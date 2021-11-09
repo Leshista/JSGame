c0d0b742216a691c3b189ca2890c6681c46be3bc
leshista <a.leshista@gmail.com>
2021-11-09 18:09:48 +0300

    Created header & imported images & music

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const header = document.querySelector('.header');
 const startWindow = document.querySelector('.start__window');
 const startButton = document.querySelector('.start__button');
 const backgroundMusic = document.querySelector('.sounds__background-music');
@@ -9,7 +10,11 @@ startButton.addEventListener('click', function () {
     startWindow.style.opacity = '0';
     setTimeout(function () {
         startWindow.style.display = 'none';
+        // startGame();
+        header.style.display = 'block';
+        setTimeout(function () {
+            header.style.opacity = '1';
+            backgroundMusic.play();
+        }, 100);
     }, 1500);
-    // startGame();
-    // backgroundMusic.play();
 });
