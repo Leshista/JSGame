1662c9257915dea36ab6f8ac874b7a96cd565e04
leshista <a.leshista@gmail.com>
2021-11-09 15:13:13 +0300

    Done with the start screen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const startWindow = document.querySelector('.start__window');
 const startButton = document.querySelector('.start__button');
-const backgroundMusic = document.querySelector('.background-music');
+const backgroundMusic = document.querySelector('.sounds__background-music');
+const buttonSound = document.querySelector('.sounds__button-sound');
 
 
 startButton.addEventListener('click', function () {
+    buttonSound.play();
+    startWindow.style.opacity = '0';
+    setTimeout(function () {
+        startWindow.style.display = 'none';
+    }, 1500);
     // startGame();
     // backgroundMusic.play();
-    startWindow.style.display = 'none';
 });
