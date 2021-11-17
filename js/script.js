6bc5d453fb2315ad3f2ef9d633d0684055c5bebf
leshista <a.leshista@gmail.com>
2021-11-17 23:21:25 +0300

    Added a way to create tilemap dynamically

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 const header = document.querySelector('.header');
 const body = document.querySelector('body');
-const main = document.querySelector('.main');
+const mainWrapper = document.querySelector('.main__wrapper');
 const startWindow = document.querySelector('.start__window');
 const startButton = document.querySelector('.start__button');
 const backgroundMusic = document.querySelector('.sounds__background-music');
 const buttonSound = document.querySelector('.sounds__button-sound');
+
+let screenWidth = window.screen.width;
+
+function screenSize() {
+    if (screenWidth <= 576) {
+        return 'xs';
+    } else if (screenWidth <= 768) {
+        return 'sm';
+    } else if (screenWidth <= 992) {
+        return 'md';
+    } else if (screenWidth <= 1200) {
+        return 'lg';
+    } else if (screenWidth <= 1400) {
+        return 'xl';
+    } else {
+        return 'xxl';
+    };
+};
+
+function createTileCols(cols, rows) {
+    for (let i = 0; i < cols; i++) {
+        let tileCol = document.createElement('div');
+        tileCol.classList.add('tile-col', `tile-col-${i + 1}`);
+        for (let i = 0; i < rows; i++) {
+            let tile = document.createElement('div');
+            tile.classList.add('tile', `${tileCol.classList[1]}__tile-${i + 1}`);
+            tileCol.appendChild(tile);
+        }
+        mainWrapper.appendChild(tileCol);
+    };
+};
+
 
 function setOpacity(element, value) {
     element.style.opacity = `${value}`;
@@ -17,16 +49,17 @@ function setDisplay(element, value) {
 startButton.addEventListener('click', function () {
     buttonSound.play();
     setOpacity(startWindow, 0);
-    setTimeout(function () {
+    setTimeout(() => {
         startWindow.remove();
         // startGame();
         setDisplay(header, 'block');
-        setDisplay(main, 'flex');
+        setDisplay(mainWrapper, 'flex');
 
-        setTimeout(function () {
+        setTimeout(() => {
             setOpacity(header, 1);
-            setOpacity(main, 1);
+            setOpacity(mainWrapper, 1);
             backgroundMusic.play();
         }, 500);
     }, 1500);
 }); // After the start button is clicked, we change opacity of the start window to 0, after this we remove it. Then we activate header and main divs, setting their display values from none to visible ones, then we change their opacity to 1 for them to be visible. We need them to turn visible a little bit later than changing their display, otherwise transitions will not work.
+createTileCols(50, 30)
