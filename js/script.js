4547c6d1c415296cee56993e43a29d473450f7e6
leshista <a.leshista@gmail.com>
2021-11-17 21:55:49 +0300

    Refacrored functions and added commas

--- js/script.js
+++ js/script.js
@@ -6,20 +6,27 @@ const startButton = document.querySelector('.start__button');
 const backgroundMusic = document.querySelector('.sounds__background-music');
 const buttonSound = document.querySelector('.sounds__button-sound');
 
+function setOpacity(element, value) {
+    element.style.opacity = `${value}`;
+}; // Change the opacity value of the DOM element
+
+function setDisplay(element, value) {
+    element.style.display = `${value}`;
+}; // Change the display value of the DOM element
 
 startButton.addEventListener('click', function () {
     buttonSound.play();
-    startWindow.style.opacity = '0';
+    setOpacity(startWindow, 0);
     setTimeout(function () {
-        startWindow.style.display = 'none';
+        startWindow.remove();
         // startGame();
-        header.style.display = 'block';
-        main.style.display = 'flex';
+        setDisplay(header, 'block');
+        setDisplay(main, 'flex');
 
         setTimeout(function () {
-            header.style.opacity = '1';
-            main.style.opacity = '1';
+            setOpacity(header, 1);
+            setOpacity(main, 1);
             backgroundMusic.play();
         }, 500);
     }, 1500);
-});
+}); // After the start button is clicked, we change opacity of the start window to 0, after this we remove it. Then we activate header and main divs, setting their display values from none to visible ones, then we change their opacity to 1 for them to be visible. We need them to turn visible a little bit later than changing their display, otherwise transitions will not work.
