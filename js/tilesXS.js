d7cd0fcc5b1b5274ea3a8331a7706c0c902e91e1
leshista <a.leshista@gmail.com>
2021-11-23 22:04:59 +0300

    Done with xs tilemap

--- js/tilesXS.js
+++ js/tilesXS.js
@@ -1,16 +1,3 @@
-//MmrmmM
-//mmrmml
-//mmrHht
-//mtRrrt
-//mtrhrrh
-//fsScrH
-//fsCCrf
-//lsCCRr
-//wsSsst
-//WlsHLo
-//brRuoo
-//Wccooo
-//oooooo
 function setTiles(screenSize) {
     setTileType(1, 1, 'mountain');
     setTileType(1, 2, 'mountains');
@@ -20,11 +7,11 @@ function setTiles(screenSize) {
     setTileType(1, 6, 'forest');
     setTileType(1, 7, 'forest');
     setTileType(1, 8, 'lake');
-    //
-    //
+    setTileType(1, 9, 'river-vertical');
+    setTileType(1, 10, 'river-vertical');
     setTileType(1, 11, 'bridge');
-    //
-    setTileType(1, 13, 'shore');
+    setTileType(1, 12, 'river-vertical');
+    setTileType(1, 13, 'river-on-water');
 
     setTileType(2, 1, 'mountains');
     setTileType(2, 2, 'mountains');
@@ -35,9 +22,9 @@ function setTiles(screenSize) {
     setTileType(2, 7, 'sroad-vertical');
     setTileType(2, 8, 'sroad-vertical');
     setTileType(2, 9, 'sroad-left-up-angle');
-    setTileType(2, 10, 'lake');
+    setTileType(2, 10, 'ruins');
     setTileType(2, 11, 'road-horizontal');
-    //
+    setTileType(2, 12, 'warehouse');
     setTileType(2, 13, 'shore');
 
     setTileType(3, 1, 'road-vertical');
@@ -46,48 +33,47 @@ function setTiles(screenSize) {
     setTileType(3, 4, 'road-vertical-right');
     setTileType(3, 5, 'road-vertical');
     setTileType(3, 6, 'sroad-right-up-angle');
-    //
-    //
+    setTileType(3, 7, 'town-left-up');
+    setTileType(3, 8, 'town-left-down');
     setTileType(3, 9, 'scrossroad');
     setTileType(3, 10, 'sroad-vertical');
     setTileType(3, 11, 'road-vertical-left');
-    //
-    setTileType(3, 13, 'shore');
+    setTileType(3, 12, 'port');
+    setTileType(3, 13, 'docks');
 
     setTileType(4, 1, 'mountains');
     setTileType(4, 2, 'mountains');
-    //
+    setTileType(4, 3, 'farm');
     setTileType(4, 4, 'road-horizontal');
     setTileType(4, 5, 'hill');
-    //
-    //
-    //
+    setTileType(4, 6, 'camp');
+    setTileType(4, 7, 'town-right-up');
+    setTileType(4, 8, 'town-right-down');
     setTileType(4, 9, 'sroad-horizontal');
     setTileType(4, 10, 'hills');
-    //
+    setTileType(4, 11, 'ruins');
     setTileType(4, 12, 'shore-corner');
     setTileType(4, 13, 'shore-inverse-corner');
 
     setTileType(5, 1, 'mountains');
     setTileType(5, 2, 'mountains');
-    //
+    setTileType(5, 3, 'house');
     setTileType(5, 4, 'sroad-right-down-angle');
     setTileType(5, 5, 'road-vertical');
     setTileType(5, 6, 'road-vertical');
     setTileType(5, 7, 'road-vertical');
     setTileType(5, 8, 'road-vertical-right');
     setTileType(5, 9, 'sroad-right-up-angle');
-    //
+    setTileType(5, 10, 'lighthouse');
     setTileType(5, 11, 'shore-corner');
     setTileType(5, 12, 'shore-inverse-corner');
     setTileType(5, 13, 'water');
-
 
     setTileType(6, 1, 'mountain');
     setTileType(6, 2, 'lake');
     setTileType(6, 3, 'trees');
     setTileType(6, 4, 'trees');
-    //
+    setTileType(6, 5, 'mill');
     setTileType(6, 6, 'hills');
     setTileType(6, 7, 'forest');
     setTileType(6, 8, 'road-horizontal');
