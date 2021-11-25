3d7beea36d1b6c7643a342e06def9e8b28d42ee6
leshista <a.leshista@gmail.com>
2021-11-25 11:47:53 +0300

    Fixed a visual bug

--- js/createEnemy.js
+++ js/createEnemy.js
@@ -5,5 +5,5 @@ function createEnemy(allTiles) {
         const houses = document.querySelectorAll('.house, .farm, .mill, .lighthouse, .warehouse, .docks');
         let randomHouse = Math.floor(Math.random() * houses.length);
         let rat = new Creature('rat', houses[randomHouse].offsetLeft / 10, houses[randomHouse].offsetTop / 10, houses[randomHouse].offsetWidth / 10, houses[randomHouse].offsetHeight / 10);
-    }
+    };
 };
